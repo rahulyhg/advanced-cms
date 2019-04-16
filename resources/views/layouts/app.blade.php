19838dc93ad841d62cdadf5e56472b53a700bbf4
wahyaumau <wahyaumau@gmail.com>
2019-04-16 21:00:03 +0700

    fixing jquery in manage.permissions.create

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,18 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                             @endif
-                            @else                            
+                            @else
+                            @role('superadministrator')
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('manage.permissions.index') }}">{{ __('Manage Permission') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('manage.roles.index') }}">{{ __('Manage Role') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('manage.users.index') }}">{{ __('Manage User') }}</a>
+                            </li>
+                            @endrole
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
